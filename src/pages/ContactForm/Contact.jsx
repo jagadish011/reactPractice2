934086c95d89ef1a94180557a638fdcfef584acf
jagadish011 <munavallijagadish@gmail.com>
2025-02-02 19:04:37 +0530

    changes in the contact form

--- src/pages/ContactForm/Contact.jsx
+++ src/pages/ContactForm/Contact.jsx
@@ -17,7 +17,9 @@ const Contact = () => {
                 message
             });
 
-            if (res.status === 201){
+            console.log("Response from the server", res)
+
+            if (res.status === 201) {
                 setStatus("Message sent successfully")
                 setName("");
                 setEmail("");
@@ -29,18 +31,18 @@ const Contact = () => {
             setStatus("Error in submitting the form")
         }
     }
-  return (
-    <div className='max-w-md mx-auto mt-10 p-6 border rounded-lg shadow-lg'>
-        <h2 className='text-xl font-semibold mb-4'>Contact Us</h2>
-        {status && <p className='text-xl font-semibold mb-4'> {status}</p>}
-        <form className='space-y-4' onSubmit={handleSubmit}>
-            <input type="text" name='name' placeholder='Enter your name' required className='w-full p-2 border rounded'/>
-            <input type="email" name='email' placeholder='Enter your email' required className='w-full p-2 border rounded'/>
-            <textarea name="message" placeholder='Enter your message' rows={4} className='w-full p-2 border rounded'></textarea>
-            <button type='submit' className='rounded p-2 w-1/2 bg-blue-400 shadow-md font-semibold text-white'>Send</button>
-        </form>
-    </div>
-  )
+    return (
+        <div className='max-w-md mx-auto mt-10 p-6 border rounded-lg shadow-lg'>
+            <h2 className='text-xl font-semibold mb-4'>Contact Us</h2>
+            {status && <p className='text-xl font-semibold mb-4'> {status}</p>}
+            <form className='space-y-4' onSubmit={handleSubmit}>
+                <input type="text" name='name' placeholder='Enter your name' required className='w-full p-2 border rounded' value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="email" name='email' placeholder='Enter your email' required className='w-full p-2 border rounded' value={email} onChange={(e) => setEmail(e.target.value)} />
+                <textarea name="message" placeholder='Enter your message' rows={4} className='w-full p-2 border rounded' value={message} onChange={(e)=> setMessage(e.target.value)}></textarea>
+                <button type='submit' className='rounded p-2 w-1/2 bg-blue-400 shadow-md font-semibold text-white'>Send</button>
+            </form>
+        </div>
+    )
 }
 
 export default Contact
