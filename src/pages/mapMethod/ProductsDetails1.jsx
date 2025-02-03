26d1a526e17deec9125503f3a37b726fdd8ee5ab
jagadish011 <munavallijagadish@gmail.com>
2025-02-03 12:05:46 +0530

    product table update

--- src/pages/mapMethod/ProductsDetails1.jsx
+++ src/pages/mapMethod/ProductsDetails1.jsx
@@ -4,6 +4,44 @@ import { FaStar } from 'react-icons/fa';
 
 const ProductsDetails1 = () => {
     const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [search, setSearch] = useState("");
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        const debounce = setTimeout(() => {
+            const result = products.filter((product) => product.category.includes(search.toLocaleLowerCase()));
+            console.log("search result" , result)
+            setSearchResults(result)
+        },500)
+
+        return () => clearTimeout(debounce);
+    },[search, products])
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value);
+    }
+
+    const productsPerPage = 5;
+
+    const indexOfLastProduct = currentPage * productsPerPage;
+    const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+
+    const paginationProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+    const totalPages = Math.ceil(products.length / productsPerPage);
+
+    const handleNext = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+    const hadlePrev = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
 
     const fetchProducts = async () => {
         try {
@@ -21,6 +59,14 @@ const ProductsDetails1 = () => {
 
     return (
         <div className='container mx-auto mt-10'>
+            <div>
+                <input type="text" value={search} onChange={handleSearch} placeholder='enter category to search' className='border p-2 my-3 rounded' />
+                {search && searchResults.map((product) => (
+                    <div key={product.id} className='bg-gray-200 p-2 rounded'>
+                        <p>{product.category}</p>
+                    </div>
+                ))}
+            </div>
             <table className='min-w-full bg-white border border-gray-300'>
                 <thead>
                     <tr className='bg-gray-200 text-gray-600 uppercase text-sm leading-normal'>
@@ -32,33 +78,31 @@ const ProductsDetails1 = () => {
                     </tr>
                 </thead>
                 <tbody className='text-gray-600 text-sm font-light'>
-                    {products.map((product) => (
+                    {searchResults.map((product) => (
                         <tr key={product.id} className='border-b border-gray-200 hover:bg-gray-100'>
                             <td className='py-3 px-6 text-left whitespace-nowrap'>{product.id}</td>
                             <td className='py-3 px-6 text-left'>{product.title}</td>
                             <td className='py-3 px-6 text-left'>{product.category}</td>
                             <td className='py-3 px-6 text-left'>{product.price}</td>
                             <td className='py-3 px-6 text-left'>
-                                {Array(Math.floor(product.rating.rate))
-                                    .fill()
-                                    .map((_, i) => (
-                                        < FaStar key={`full-${i}`} style={{ color: 'gold' }} />
-                                    ))
-                                }
-                                {product.rating.rate % 1 != 0 && (
-                                    < FaStar style={{ color: 'gold', opacity: 0.5 }} />
-                                )}
-                                {Array(5 - Math.ceil(product.rating.rate))
-                                    .fill()
-                                    .map((_, i) => (
-                                        < FaStar key={`empty-${i}`} style={{ color: 'gray', opacity: 0.5 }} />
+                                {
+                                    [...Array(5)].map((_, i) => (
+                                        <FaStar key={i}
+                                            style={
+                                                { color: i < Math.floor(product.rating.rate) ? 'gold' : i < Math.ceil(product.rating.rate) ? 'gold' : 'gray' }
+                                            } />
                                     ))
                                 }
                             </td>
+
                         </tr>
                     ))}
                 </tbody>
             </table>
+            <div className='flex justify-between mt-5'>
+                <button className='bg-blue-500 text-white rounded p-2 ' onClick={hadlePrev}>Prev</button>
+                <button className='bg-red-500 text-white rounded p-2 ' onClick={handleNext}>Next</button>
+            </div>
         </div>
     )
 }
